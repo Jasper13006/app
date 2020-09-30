c7e1091233694e748ec02e84e3677ead99447484
unknown <jasper13006@gmail.com>
2020-09-30 17:22:34 -0300

    cambios copados con el useState

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,21 @@ import React from 'react';
 import PokeCard from './PokeCard'
 import pokeData from './pokeData'
 import './Pokedex.css'
+import { useState } from 'react';
 
 const Pokedex = (props) => {
     const { history } = props
+    const [count, setCount] = useState(0);
     // console.log(pokeData)
     return (
         <div >
-            <h1>Esta fuck esta andando</h1>
+            <h1>Esta app esta funcionando</h1>
+            <div className='container'>
+                <p>You clicked {count} times</p>
+                <button onClick={() => setCount(count + 1)}>
+                    Click me
+                </button>
+            </div>
             <div className='divDelPokedex'>
                 {Object.keys(pokeData).map(pokemonId =>
                     <PokeCard pokeData={pokeData[pokemonId]} history={history} />
